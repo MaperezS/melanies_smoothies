afdf7f33ed8ca4a9852ba14505ecca2623a182e9
MaperezS <maperez@seidoranalytics.com>
2025-05-28 17:54:52 -0300

    Update streamlit_app.py
    Pongamos el JSON en un marco de datos
    Se suele usar "df " como abreviatura de "dataframe". Llamaremos a nuestro dataframe sf_df , porque es nuestro Dataframe de SmoothieFroot.

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,5 @@ if ingredients_list:
 #nueva sección para llamar a la api de smoothiefroot
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
